de6b83ec63b6fad38c96f1edcfaf32259a8f8d97
anish <brt.anishlap@gmail.com>
2026-06-21 09:34:29 +0545

    stack push pop

---
+++ Lab 3/dsa stack push pop.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#define MAX 5
+using namespace std;
+
+int stack[MAX];
+int top=-1;
+
+void push (int value){
+    if(top == MAX-1){
+        cout<<"Stack overflow"<<endl;
+    }
+    else{
+        top++;
+        stack[top]=value;
+        cout<<value<<" is pushed into the stack"<<endl;
+    }
+}
+
+void pop(){
+    if(top == -1){
+        cout<<"Stack underflow"<<endl;
+    }
+    else{
+        cout<<stack[top]<<" is popped from the stack"<<endl;
+        top--;  
+    }
+}
+
+void display(){
+    if(top == -1){
+        cout<<"Stack is empty"<<endl;
+    }
+    else{
+        cout<<"Stack elements are: ";
+        for(int i=top; i>=0; i--){
+            cout<<stack[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+    pop();
+
+    display();
+
+    return 0;
+}
